882eea1205e3a65156a89f7380625852462dbbaa
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-06 19:11:43 +0200

    Update database.h

--- database/database.h
+++ database/database.h
@@ -1,37 +1,47 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+#include <QObject>
 #include <QSqlDatabase>
-#include <memory>
+#include <QSqlError>
 
 #include "../operation/fileread.h"
 
-class Database
+class Database : public QObject
 {
+    Q_OBJECT
 public:
     Database()
     {
         QScopedPointer<FileRead> fileRead(new FileRead("db.cfg"));
         QVector<QString> dbConfig = fileRead->readFile();
-        db_.setHostName(dbConfig[0].replace("\n", ""));
-        db_.setPort(dbConfig[1].replace("\n", "").toInt());
-        db_.setDatabaseName(dbConfig[2].replace("\n", ""));
-        db_.setUserName(dbConfig[3].replace("\n", ""));
-        db_.setPassword(dbConfig[4].replace("\n", ""));
+        for(QString &line: dbConfig) {
+            line.replace("\n", "");
+            line.replace("\r", "");
+        }
+        fileRead.reset();
+        db_.setHostName(dbConfig[0]);
+        db_.setPort(3306);
+        db_.setDatabaseName(dbConfig[1]);
+        db_.setUserName(dbConfig[2]);
+        db_.setPassword(dbConfig[3]);
     }
     ~Database(){}
 
-    bool connect()
+    void connect()
     {
-        if(db_.isOpen()) {
-            return true;
+        if(db_.open()) {
+            emit connectionStatus("Połączono z bazą danych.", 5000);
         } else {
-            return false;
+            emit connectionStatus(db_.lastError().text(), 5000);
         }
     }
 
 private:
     QSqlDatabase db_ = QSqlDatabase::addDatabase("QMYSQL");
+
+signals:
+    void connectionStatus(QString, int);
 };
 
 #endif // DATABASE_H
