9c69ef1794ce14242d91e5ec2642faee87306744
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-06 19:12:28 +0200

    Update files

--- loginscreen.cpp
+++ loginscreen.cpp
@@ -1,6 +1,8 @@
 #include "loginscreen.h"
 #include "./ui_loginscreen.h"
 
+#include <QMessageBox>
+
 #include "database/database.h"
 
 LoginScreen::LoginScreen(QWidget *parent)
@@ -9,16 +11,58 @@ LoginScreen::LoginScreen(QWidget *parent)
 {
     ui->setupUi(this);
 
-    QScopedPointer<Database> db(new Database());
-    if(db->connect()) {
-        ui->statusbar->showMessage("Połączono z bazą danych");
-    } else {
-        ui->statusbar->showMessage("Nie udało się połączyć z bazą danych");
-    }
+    connect(ui->pushButtonExit, &QPushButton::clicked, this, &LoginScreen::close);
+    connect(ui->pushButtonLogIn, &QPushButton::clicked, this, &LoginScreen::showMainWindow);
+
+    database();
+    getLastUserFromFile({"asd"});
 }
 
 LoginScreen::~LoginScreen()
 {
     delete ui;
+}
+
+void LoginScreen::database()
+{
+    QScopedPointer<Database> db(new Database());
+    connect(db.get(), &Database::connectionStatus, ui->statusbar, &QStatusBar::showMessage);
+    db->connect();
+}
+
+void LoginScreen::showMainWindow()
+{
+    setCurrentUserToFile("asd");
+    ui->statusbar->showMessage("Show Main Window");
+}
+
+void LoginScreen::updateUsers()
+{
+    ui->statusbar->showMessage("Update users");
+}
+
+void LoginScreen::setCurrentUserToFile(QString currentUser)
+{
+    ui->statusbar->showMessage("Save current user to file");
+}
+
+void LoginScreen::getLastUserFromFile(const QVector<QString> &lastUser)
+{
+    ui->statusbar->showMessage("Get last user from file");
+}
+
+void LoginScreen::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return)
+        showMainWindow();
+}
+
+void LoginScreen::closeEvent(QCloseEvent *event)
+{
+    QMessageBox::StandardButton reply = QMessageBox::question(this, "Informacja", "Czy na pewno chcesz zamknąć aplikację?", QMessageBox::Yes|QMessageBox::No);
+    if(reply == QMessageBox::Yes)
+        event->accept();
+    else
+        event->ignore();
 }
 
--- loginscreen.h
+++ loginscreen.h
@@ -1,6 +1,8 @@
 #ifndef LOGINSCREEN_H
 #define LOGINSCREEN_H
 
+#include <QKeyEvent>
+#include <QCloseEvent>
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
@@ -17,5 +19,15 @@ public:
 
 private:
     Ui::LoginScreen *ui;
+
+    void database();
+    void showMainWindow();
+
+    void updateUsers();
+    void setCurrentUserToFile(QString currentUser);
+    void getLastUserFromFile(const QVector<QString> &lastUser);
+
+    void keyPressEvent(QKeyEvent *event);
+    void closeEvent(QCloseEvent *event);
 };
 #endif // LOGINSCREEN_H
