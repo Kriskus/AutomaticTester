9e38d4e509f2d8960c5e15e1305b9b68e8f6298c
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-06 20:17:37 +0200

    Update loginscreen.cpp

--- loginscreen.cpp
+++ loginscreen.cpp
@@ -1,11 +1,20 @@
 #include "loginscreen.h"
 #include "./ui_loginscreen.h"
 
+#include "database/database.h"
+
 LoginScreen::LoginScreen(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::LoginScreen)
 {
     ui->setupUi(this);
+
+    QScopedPointer<Database> db(new Database());
+    if(db->connect()) {
+        ui->statusbar->showMessage("Połączono z bazą danych");
+    } else {
+        ui->statusbar->showMessage("Nie udało się połączyć z bazą danych");
+    }
 }
 
 LoginScreen::~LoginScreen()
