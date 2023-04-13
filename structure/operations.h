22e381b9f8d6f0e5bbf877fbaa1b62a80d468b55
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-13 13:59:24 +0200

    Update operations.h

--- structure/operations.h
+++ structure/operations.h
@@ -1,4 +1,31 @@
 #ifndef OPERATIONS_H
 #define OPERATIONS_H
 
+#include <QString>
+
+class Operation {
+public:
+    Operation(unsigned int id, unsigned int idTestcase, unsigned int idOrder, unsigned int queue, const QString& name)
+        : id_(id)
+        , idTestcase_(idTestcase)
+        , idOrder_(idOrder)
+        , queue_(queue)
+        , name_(name)
+    {}
+    ~Operation() {}
+
+    unsigned int getId() const { return id_; }
+    unsigned int getIdTestcase() const { return idTestcase_; }
+    unsigned int getIdOrder() const { return idOrder_; }
+    unsigned int getqueue() const { return queue_; }
+    QString getName() { return name_; }
+
+private:
+    unsigned int id_{};
+    unsigned int idTestcase_{};
+    unsigned int idOrder_{};
+    unsigned int queue_{};
+    QString name_{};
+};
+
 #endif // OPERATIONS_H
