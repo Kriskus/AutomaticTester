98cc68af7a3219d14ab84545b9bfa08b14b4506a
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-13 13:59:30 +0200

    Update sequence.h

--- structure/sequence.h
+++ structure/sequence.h
@@ -1,4 +1,22 @@
 #ifndef SEQUENCE_H
 #define SEQUENCE_H
 
+#include <QString>
+
+class Sequence {
+public:
+    Sequence(unsigned int id, const QString& name)
+        : id_(id)
+        , name_(name)
+    {}
+    ~Sequence() {}
+
+    unsigned int getId() const { return id_; }
+    QString getName() { return name_; }
+
+private:
+    unsigned int id_{};
+    QString name_{};
+};
+
 #endif // SEQUENCE_H
