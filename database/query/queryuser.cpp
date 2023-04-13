4c2924a630acc48176a93ff4c0e9dd4596c8a52b
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-13 13:59:19 +0200

    Create queryuser.cpp

---
+++ database/query/queryuser.cpp
@@ -0,0 +1,31 @@
+#include "queryuser.h"
+
+QueryUser::QueryUser()
+{
+
+}
+
+QueryUser::~QueryUser()
+{
+
+}
+
+void QueryUser::userSELECT()
+{
+
+}
+
+void QueryUser::userINSERT()
+{
+
+}
+
+void QueryUser::userDELETE()
+{
+
+}
+
+void QueryUser::userUPDATE()
+{
+
+}
