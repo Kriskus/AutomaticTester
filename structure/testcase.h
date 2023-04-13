52ea9ead29cbd5d84528121d319216d7e860e010
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-13 13:59:33 +0200

    Update testcase.h

--- structure/testcase.h
+++ structure/testcase.h
@@ -1,4 +1,31 @@
 #ifndef TESTCASE_H
 #define TESTCASE_H
 
+#include <QString>
+
+class Testcase {
+public:
+    Testcase(unsigned int id, unsigned int idScenario, unsigned int idUserCreator, unsigned int idUserConfirmer, const QString& name)
+        : id_(id)
+        , idScenario_(idSequence)
+        , idUserCreator_(idUserCreator)
+        , idUserConfirmer_(idUserConfirmer)
+        , name_(name)
+    {}
+    ~Testcase() {}
+
+    unsigned int getId() const { return id_; }
+    unsigned int getIdScenario() const { return idScenario_; }
+    unsigned int getIdUserCreator() const { return idUserCreator_; }
+    unsigned int getIdUserConfirmer() const { return idUserConfirmer_; }
+    QString getName() { return name_; }
+
+private:
+    unsigned int id_{};
+    unsigned int idScenario_{};
+    unsigned int idUserCreator_{};
+    unsigned int idUserConfirmer_{};
+    QString name_{};
+};
+
 #endif // TESTCASE_H
