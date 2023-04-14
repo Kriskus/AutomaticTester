97764b885347aaa7a15267e35927089ad965d346
Kriskus <krzysztofkkusmierczyk@gmail.com>
2023-04-14 14:00:58 +0200

    Update queryuser.cpp

--- database/query/queryuser.cpp
+++ database/query/queryuser.cpp
@@ -1,5 +1,7 @@
 #include "queryuser.h"
 
+#include <QDebug>
+
 QueryUser::QueryUser()
 {
 
@@ -12,7 +14,10 @@ QueryUser::~QueryUser()
 
 void QueryUser::userSELECT()
 {
-
+    query_.exec("SELECT * FROM Uzytkownicy;");
+    while(query_.next()) {
+        users.push_back(std::make_shared<User> (query_.value(0).toInt(), query_.value(1).toString(), query_.value(2).toString(), query_.value(4).toBool()));
+    }
 }
 
 void QueryUser::userINSERT()
